c155ac649489f50dc6579f560a589f850a67b941
Jared Lunde <jared.lunde@gmail.com>
2019-02-26 11:45:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# trie-memoize
+A memoizer in which each function argument represents a new key, creating a trie of caches
+as defined by your setup.
+
+## Installation
+`yarn add trie-memoize` or `npm i trie-memoize`
+
+## Usage
+```js
+const m1 = memoize([{}], v => v.toUpperCase())
+m1('foo')  // FOO
+
+const m2 = memoize([{}, Map], (v1, v2) => `${v1}-${v2}`)
+m2('foo', 'bar')  // foo-bar
+
+const m3 = memoize(
+  [WeakMap, WeakMap, Map], 
+  (v1, v2, v3) => `${JSON.stringify(v1)}-${JSON.stringify(v1)}-${JSON.stringify(v3)}`
+)
+let v1 = {}, v2 = {}, v3 = 'foo'
+m3(v1, v2, v3)  // {}-{}-"foo"
+```
+
+### `memoize(caches <Array>, fn <Function>)`
+- `caches` `<Array>`
+    - An array of plain objects or constructors used for caching each level of the tree.
+      The first array element will be the cache for the first argument of the function,
+      call, and so on. Therefore, the length of this array must be the same as the 
+      length of arguments your memoized function accepts.
+    - You may provide your own cache (e.g. not `Map`, `WeakMap`, `{}`). The only requirement
+      is that it has `get` and `set` methods.
+- `fn` `<Function>` 
+    - The function being memoized
---
+++ src/index.js
@@ -0,0 +1,99 @@
+import isPlainObject from 'is-plain-object'
+
+
+const createCache = obj => {
+  if (isPlainObject(obj)) {
+    return {
+      get: k => obj[k],
+      set: (k, v) => {
+        obj[k] = v
+        return v
+      }
+    }
+  }
+
+  return new obj()
+}
+
+const Cache = constructors => {
+  const depth = constructors.length
+  const baseCache = createCache(constructors[0])
+  let get, set
+
+  // quicker access for one and two-argument functions
+  if (depth === 1) {
+    get = k => baseCache.get(k[0])
+    set = (k, v) => {
+      baseCache.set(k[0], v)
+      return v
+    }
+  }
+  else if (depth === 2) {
+    get = args => {
+      const base = baseCache.get(args[0])
+      return base === void 0 ? base : base.get(args[1])
+    }
+
+    set = (args, value) => {
+      const base = baseCache.get(args[0])
+      if (base === void 0) {
+        const map = createCache(constructors[1])
+        map.set(args[1], value)
+        baseCache.set(args[0], map)
+      }
+      else {
+        base.set(args[1], value)
+      }
+
+      return value
+    }
+  }
+  else {
+    let i
+    
+    get = args => {
+      let node = baseCache
+      
+      for (i = 0; i < args.length; i++) {
+        node = node.get(args[i])
+        if (node === void 0) return
+      }
+      
+      return node
+    }
+
+    set = (args, value) => {
+      let node = baseCache
+
+      for (i = 0; i < args.length - 1; i++) {
+        let map = node.get(args[i])
+        
+        if (map === void 0) {
+          map = createCache(constructors[i])
+          node.set(args[i], map)
+          node = map
+        }
+        else {
+          node = map
+        }
+      }
+
+      node.set(args[args.length - 1], value)
+      return value
+    }
+  }
+
+  return {get, set}
+}
+
+
+function memoize (mapConstructors, fn) {
+  const cache = Cache(mapConstructors)
+
+  return function () {
+    const item = cache.get(arguments)
+    return item === void 0 ? cache.set(arguments, fn.apply(this, arguments)) : item
+  }
+}
+
+export default memoize
